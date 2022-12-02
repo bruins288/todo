03128308b4964b6c1b5f6e5d4743d3d91c35a217
bruins288 <bruins288@gmail.com>
2022-12-02 09:52:06 +0300

    Добавлена привязка по имени файла

--- src/App.js
+++ src/App.js
@@ -7,13 +7,39 @@ function App() {
       <div className="todo__sidebar">
         <List
           items={[
-            { id: 1, name: "Все задачи", active: true },
-            { id: 2, name: "Фронтенд" },
-            { id: 3, name: "Фильмы и сериалы" },
-            { id: 4, name: "Книги" },
-            { id: 5, name: "Дети" },
-            { id: 6, name: "Личное" },
+            {
+              id: 1,
+              name: "Все задачи",
+              iconFileName: "alltasks.png",
+              active: true,
+            },
           ]}
+          isRemovable={true}
+        />
+        <List
+          items={[
+            {
+              id: 1,
+              name: "Фронтенд",
+              iconFileName: "react.webp",
+              allTasksId: 1,
+            },
+            {
+              id: 2,
+              name: "Фильмы и сериалы",
+              iconFileName: "films.webp",
+              allTasksId: 1,
+            },
+            { id: 3, name: "Книги", iconFileName: "books.webp", allTasksId: 1 },
+            { id: 4, name: "Дети", iconFileName: "childs.webp", allTasksId: 1 },
+            {
+              id: 5,
+              name: "Личное",
+              iconFileName: "personals.webp",
+              allTasksId: 1,
+            },
+          ]}
+          isRemovable={true}
         />
       </div>
       <div className="todo__tasks"></div>
